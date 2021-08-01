c23183a6c9c11e4c8c97e74f7e9c631cfb303f6c
Quang Hoang <quang@appsmiths.com>
2021-08-01 16:52:41 +0700

    model

--- pythagoras/models.py
+++ pythagoras/models.py
@@ -3,51 +3,37 @@ from ckeditor.fields import RichTextField
 
 # Create your models here.
 
-
-class LifePath(models.Model):
-    life_path_number = models.IntegerField(primary_key=True, unique=True)
+class MeaningModel(models.Model):
+    model_number = models.IntegerField(primary_key=True, unique=True)
+    key_info = models.CharField(max_length=200)
     description = RichTextField()
     meaning = RichTextField()
-    
-    def __str__(self):
-        return self.meaning
 
+    def __str__(self):
+        return self.key_info
+        
+    class Meta:
+        abstract = True
+
+class LifePath(MeaningModel):   
     class Meta:    
         verbose_name = 'SỐ CHỦ ĐẠO'
         verbose_name_plural = 'CHỈ SỐ CHỦ ĐẠO'
 
 
-class DestinyPath(models.Model):
-    destiny_path_number = models.IntegerField(primary_key=True, unique=True)
-    description = RichTextField()
-    meaning = RichTextField()    
-
-    def __str__(self):
-        return self.meaning
-
+class DestinyPath(MeaningModel):
     class Meta:    
         verbose_name = 'SỨ MỆNH'
         verbose_name_plural = 'CHỈ SỐ SỨ MỆNH'
 
 
-class HearthDesire(models.Model):
-    hearth_desire_number = models.IntegerField(primary_key=True ,unique=True)    
-    description = RichTextField()
-    meaning = RichTextField()
-
-    def __str__(self):
-        return self.meaning
-    
+class HearthDesire(MeaningModel):
     class Meta:    
         verbose_name = 'LINH HỒN'
         verbose_name_plural = 'CHỈ SỐ LINH HỒN'     
 
 
-class Personality(models.Model):
-    personality_number = models.IntegerField(primary_key=True, unique=True)
-    description = RichTextField() 
-    meaning = RichTextField()       
-
+class Personality(MeaningModel):
     def __str__(self):
         return self.meaning
 
@@ -56,48 +42,24 @@ class Personality(models.Model):
         verbose_name_plural = 'CHỈ SỐ NHÂN CÁCH'   
 
 
-class PowerPath(models.Model):
-    power_number = models.IntegerField(primary_key=True, unique=True)
-    description = RichTextField()
-    meaning = RichTextField()    
-
-    def __str__(self):
-        return self.meaning
-
+class PowerPath(MeaningModel):
     class Meta:    
         verbose_name = 'SỐ TRƯỞNG THÀNH'
         verbose_name_plural = 'CHỈ SỐ TRƯỞNG THÀNH' 
 
 
-class ActivePath(models.Model):
-    active_number = models.IntegerField(primary_key=True, unique=True)
-    description = RichTextField() 
-    meaning = RichTextField()    
-
-    def __str__(self):
-        return self.meaning
-
+class ActivePath(MeaningModel):
     class Meta:    
         verbose_name = 'TÊN HIỆN TẠI'
         verbose_name_plural = 'CHỈ SỐ TÊN HIỆN TẠI' 
 
 
-class LegacyPath(models.Model):
-    legacy_number = models.IntegerField(primary_key=True, unique=True)
-    description = RichTextField() 
-    meaning = RichTextField()    
-
-    def __str__(self):
-        return self.meaning
-
+class LegacyPath(MeaningModel):
     class Meta:    
-        verbose_name = 'Legacy Path'
+        verbose_name = 'Legac'
         verbose_name_plural = 'Legacy Path' 
 
-class ExpressionPath(models.Model):
-    expression_number = models.IntegerField(primary_key=True, unique=True)
-    description = RichTextField() 
-    meaning = RichTextField()    
-
-    def __str__(self):
-        return self.meaning
+class ExpressionPath(MeaningModel):
+    class Meta:    
+        verbose_name = 'Expression '
+        verbose_name_plural = 'Expression Path' 
